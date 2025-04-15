88627f8f3f27daddea3aa0fd0295396d13702f3f
Giulia Rizzo <girzzo@gmail.com>
2025-04-15 16:53:52 +0200

    Update MyNavbar.tsx

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,21 +1,27 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
+import { Link, useLocation } from "react-router-dom"
 
 const MyNavbar = function () {
+  const location = useLocation()
   return (
     <Navbar expand="lg" className="prince text-white">
       <Container>
-        <Navbar.Brand href="#home" className=" text-white">
+        <Link className="navbar-brand text-white" to="/">
           <em>Armageddon</em>
-        </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
         <Navbar.Collapse id="basic-navbar-nav " className=" text-white">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className=" text-white">
+            <Link
+              className={
+                location.pathname === "/"
+                  ? "nav-link active text-white"
+                  : "nav-link text-white-50"
+              }
+              to="/"
+            >
               Home
-            </Nav.Link>
-            <Nav.Link href="#link" className=" text-white">
-              Link
-            </Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
